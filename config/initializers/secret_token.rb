edcaf0854009f782f94a12de8185a3e0deced2b4
Dan Collis-Puro <djcp@thoughtbot.com>
2013-05-23 11:41:54 -0400

    Deploy a secure secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,10 @@
-# Be sure to restart your server when you modify this file.
+# Technique Courtesy of:
+# http://daniel.fone.net.nz/blog/2013/05/20/a-better-way-to-manage-the-rails-secret-token/
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Chill::Application.config.secret_token = '180a779f131debb67daa84ca2bb02feb0b7e2a0c9d932b59dc7c3b6f9afa89a095475823bbb4ba6dd6041f039e43309f1ecc6f462f7d02763db57aa2add43346'
+secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
+
+Chill::Application.config.secret_token = secret_token
