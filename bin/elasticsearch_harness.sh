c690ff2bd868a34f54965d5944abd50b3e4bfb0b
patrick brisbin <pbrisbin@gmail.com>
2013-06-12 16:17:33 -0400

    Quiet elasticsearch setup script

--- bin/elasticsearch_harness.sh
+++ bin/elasticsearch_harness.sh
@@ -4,15 +4,19 @@ ELASTICSEARCH_RUNNING=`lsof -i:9200`
 
 if [ "$ELASTICSEARCH_RUNNING" ];
 then
+  echo 'Elastic search already running'
   exit 0
 fi
 
 start_elasticsearch() {
-  cd ./tmp/elasticsearch && exec ./bin/elasticsearch -f
+  echo 'Starting elasticsearch...'
+  cd elasticsearch
+  exec ./bin/elasticsearch -f
 }
 
 write_elasticsearch_config() {
-cat > elasticsearch/config/elasticsearch.yml <<End-of-config
+  echo 'Writing elasticsearch config...'
+  cat > elasticsearch/config/elasticsearch.yml <<End-of-config
 
 cluster.name: chillingeffects
 discovery.zen.ping.multicast.enabled: false
@@ -20,18 +24,27 @@ discovery.zen.ping.multicast.enabled: false
 End-of-config
 }
 
-if [ -d './tmp/elasticsearch' ]
+install_elasticsearch() {
+  echo 'Installing elasticsearch under ./tmp...'
+  curl --silent --output elasticsearch.tgz \
+    'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.tar.gz'
+
+  tar xfz elasticsearch.tgz
+
+  mv elasticsearch-0.90.1 elasticsearch
+}
+
+set -e
+
+cd ./tmp
+
+if [ -d './elasticsearch' ]
 then
   start_elasticsearch
 else
-  cd ./tmp
-  curl https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.tar.gz \
-    -o elasticsearch.tgz
-  tar xvfz elasticsearch.tgz
-  mv elasticsearch-0.90.1 elasticsearch
+  install_elasticsearch
 
   write_elasticsearch_config
 
-  cd ..
   start_elasticsearch
 fi
