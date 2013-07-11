1d605400caa9f0058e25b7129ac388870ba26cff
Dan Collis-Puro <djcp@thoughtbot.com>
2013-07-11 15:27:25 -0400

    Add login toggle view spec

---
+++ spec/views/shared/_footer.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'shared/_footer.html.erb' do
+  include Devise::TestHelpers
+
+  context 'signed in' do
+    it 'gives a link to the admin' do
+      controller.stub(user_signed_in?: true)
+
+      render
+
+      expect(page).to have_css('a', text: 'Admin')
+    end
+
+  end
+
+  context 'signed out' do
+    it 'gives a link to sign in' do
+      render
+
+      expect(page).to have_css('a', text: 'Sign In')
+    end
+
+  end
+end
