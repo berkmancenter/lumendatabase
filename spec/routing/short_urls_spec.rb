b36b2daefb432d2fc57b2a59fe964337b8873eaa
Dan Collis-Puro <djcp@thoughtbot.com>
2013-07-17 15:44:02 -0400

    Case-insensitive short notice URLs

--- spec/routing/short_urls_spec.rb
+++ spec/routing/short_urls_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe "Notices" do
-  it "routes /n/notice_id to NoticesController#show" do
-    expect(get: '/n/1234').to route_to(
-      controller:  "notices",
-      action: "show",
-      id: "1234"
+  ['n', 'N'].each do |base_path_fragment|
+    it "routes /#{base_path_fragment}/notice_id to NoticesController#show" do
+      expect(get: "/#{base_path_fragment}/1234").to route_to(
+        controller:  "notices",
+        action: "show",
+        id: "1234"
     )
+    end
   end
 
 end
