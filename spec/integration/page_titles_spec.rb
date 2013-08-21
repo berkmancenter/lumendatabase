09bd6e92df488d61f962a04a65ae7eb4a6110a88
patrick brisbin <pbrisbin@gmail.com>
2013-08-21 15:35:22 -0400

    Add dynamic page titles and navigational headings

---
+++ spec/integration/page_titles_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+feature 'Page titles' do
+  scenario "Home" do
+    visit '/'
+
+    expect(page).to have_exact_title('Chilling Effects')
+  end
+
+  scenario "Search", search: true do
+    visit '/'
+    fill_in "term", with: "search terms"
+    click_on "Go"
+
+    expect(page).to have_exact_title('Search :: Chilling Effects')
+  end
+
+  scenario "Blog" do
+    blog_entry = create(:blog_entry)
+
+    visit '/'
+    click_on 'Blog'
+
+    # index
+    expect(page).to have_exact_title('Blog :: Chilling Effects')
+    expect(page).to have_heading('Blog')
+
+    click_on blog_entry.title
+
+    # show
+    expect(page).to have_exact_title("#{blog_entry.title} :: Blog :: Chilling Effects")
+    expect(page).to have_heading('Blog')
+  end
+
+  scenario "Topics", search: true do
+    category = create(:category)
+
+    visit '/'
+    click_on 'Topics'
+    click_on category.name
+
+    expect(page).to have_exact_title("#{category.name} :: Topics :: Chilling Effects")
+    expect(page).to have_heading('Topics')
+  end
+
+  scenario "Report Notice" do
+    visit '/'
+    click_on 'Report Notice'
+
+    # select type view
+    expect(page).to have_exact_title('Report Notice :: Chilling Effects')
+    expect(page).to have_heading('Report Notice')
+
+    click_on 'DMCA'
+
+    # submission form itself
+    expect(page).to have_exact_title('Dmca :: Report Notice :: Chilling Effects')
+    expect(page).to have_heading('Report Notice')
+  end
+
+  scenario "Notice" do
+    notice = create(:dmca)
+
+    visit '/'
+    click_on notice.title
+
+    expect(page).to have_exact_title("#{notice.title} :: Notices :: Chilling Effects")
+  end
+
+  scenario "Sign in" do
+    visit '/'
+    click_on "Sign In"
+
+    expect(page).to have_exact_title('Sign In :: Chilling Effects')
+  end
+
+  context "Pages" do
+    %w( About Privacy Disclaimer Researchers ).each do |area|
+      scenario area do
+        visit '/'
+        click_on area
+
+        expect(page).to have_exact_title("#{area} :: Chilling Effects")
+        expect(page).to have_heading(area)
+      end
+    end
+  end
+
+  private
+
+  def have_exact_title(expected_title)
+    have_title(/\A#{Regexp.escape(expected_title)}\Z/)
+  end
+end
---
+++ spec/support/matchers/have_heading.rb
@@ -0,0 +1,18 @@
+RSpec::Matchers.define :have_heading do |heading|
+  match do |page|
+    @page = page
+    @page.has_css?("h2:contains('#{heading}')")
+  end
+
+  failure_message_for_should do |_|
+    message = "expected page to have heading #{heading}"
+
+    if element = @page.first('h2')
+      message << ", actual heading was #{element.text}."
+    else
+      message << ", no element found."
+    end
+
+    message
+  end
+end
