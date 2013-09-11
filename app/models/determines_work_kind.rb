afcb43cb74af687147176dffa292cd27f28fa6c5
patrick brisbin <pbrisbin@gmail.com>
2013-09-11 14:44:59 -0400

    Generalize DeterminesWorkKind::Classifier
    Mainly because we'll need to call it against non-url strings soon.

--- app/models/determines_work_kind.rb
+++ app/models/determines_work_kind.rb
@@ -1,52 +1,51 @@
 class DeterminesWorkKind
   def initialize(work)
-    @primary_urls = work.copyrighted_urls.map(&:url)
-    @secondary_urls = work.infringing_urls.map(&:url)
-    @results = { unknown: 0 }
+    @work = work
   end
 
   def kind
-    classify_urls(@primary_urls, 5)
-    classify_urls(@secondary_urls, 1)
+    classifier = Classifier.new
+    classifier.classify_all(work.copyrighted_urls.map(&:url), 5)
+    classifier.classify_all(work.infringing_urls.map(&:url), 1)
 
-    pick_result
+    classifier.best
   end
 
   private
 
-  def pick_result
-    @results.max_by{|kind, weight| weight}.first
-  end
+  attr_reader :work
 
-  def classify_urls(urls, weight)
-    urls.each do |url|
-      url_classifier = ClassifyUrl.new(url, weight)
-      @results.merge!(url_classifier.classify) do |key, old_value, new_value|
-        old_value + new_value
-      end
-    end
-  end
-
-  class ClassifyUrl
+  class Classifier
     PATTERNS = {
       music: %r{mp3|aac|album|flac|song}i,
       movie: %r{mp4|mov|movies|dvd|xvid|rip|bluray}i,
       book: %r{page|novel|book|epub|kindle}i
     }
 
-    def initialize(url, weight)
-      @url = url
-      @weight = weight || 1
+    def initialize
+      @results = Hash.new { |hash,key| hash[key] = 0 }
+      @results[:unknown] = 0
     end
 
-    def classify
-      classification_results = {}
-      PATTERNS.each do |kind, pattern|
-        if @url =~ pattern
-          classification_results[kind] = @weight
+    def classify_all(values, weight)
+      values.each do |value|
+        classify(value, weight)
+      end
+    end
+
+    def classify(value, weight)
+      PATTERNS.each do |kind,pattern|
+        if value =~ pattern
+          results[kind] += weight
         end
       end
-      classification_results
     end
+
+    def best
+      results.max_by { |_,weight| weight }.first
+    end
+
+    attr_reader :results
+
   end
 end
