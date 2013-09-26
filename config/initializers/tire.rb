152b33c49828565eec4a12cbde0e6c0391a13371
Dan Collis-Puro <djcp@thoughtbot.com>
2013-09-26 15:15:11 -0400

    Create a custom RestClient to configure timeouts.
    This allows us to wait longer for elasticsearch responses, which should
    lead to fewer 500 errors when searches time out.

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,4 +1,7 @@
+require 'tire/http/chill_client'
+
 Tire.configure do
+  client Tire::HTTP::Client::ChillClient
   url ENV["ELASTICSEARCH_URL"] || "http://localhost:9200"
   wrapper SearchResultsProxy
 end
