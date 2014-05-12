a14f7130b6fc5416795b22a9ef98ffb9955bbb52
chill role account <chill-prod@cyber.law.harvard.edu>
2014-05-12 15:40:07 -0400

    chaneged to non-tls

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,9 +2,10 @@ if Rails.env.staging? || Rails.env.production?
   SMTP_SETTINGS = {
     address: ENV['SMTP_ADDRESS'], # example: 'smtp.sendgrid.net'
     authentication: :plain,
+    enable_starttls_auto: false,
     domain: ENV['SMTP_DOMAIN'], # example: 'this-app.com'
     password: ENV['SMTP_PASSWORD'],
-    port: '587',
+    port: ENV['SMTP_PORT'],
     user_name: ENV['SMTP_USERNAME']
   }
 end
