3efcd22fc967ee21652f787f87c981d38d596ba1
Dan Collis-Puro <djcp@thoughtbot.com>
2014-05-13 12:01:29 -0400

    Naively count rows for large tables to speed UI significantly
    Monkeypatch Kaminari::ActiveRecordRelationMethods to naively count
    configured tables via reltuples, which - while imprecise - returns
    almost instantly as opposed to taking multiple minutes.
    
    This feature defaults to only intercept counts on  `infringing_urls`,
    you can configure it by modifying the following ENV with a
    comma-separated list:
    
        TABLES_USING_NAIVE_COUNTS="infringing_urls,works"
    
    See also:
    
    * https://wiki.postgresql.org/wiki/Slow_Counting

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,5 +1,10 @@
+require './lib/kaminari/active_record_relation_methods'
+
 # Prevents conflicts between Kaminari (used by rails_admin) and
 # will_paginate.
 Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
+
+Chill::Application.config.tables_using_naive_counts =
+  (ENV['TABLES_USING_NAIVE_COUNTS'] || 'infringing_urls').split(',')
