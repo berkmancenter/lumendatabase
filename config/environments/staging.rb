f098d278b44b00fe38490d5ed6b9cfbb6391297a
Dan Collis-Puro <djcp@thoughtbot.com>
2014-05-14 16:30:02 -0400

    Make outgoing email domain configurable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,5 +70,5 @@ Chill::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { host: 'chillingeffects.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: (ENV['EMAIL_DOMAIN'] || 'chillingeffects.org') }
 end
