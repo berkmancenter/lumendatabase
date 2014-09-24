50e44bc3ebd8f39f1fcf26c2dd90c3d06fe3c8d3
jdcc <jclark@cyber.law.harvard.edu>
2014-09-24 12:02:48 -0400

    there are multiple google emails

--- db/migrate/20140916173139_add_publication_delay_to_user.rb
+++ db/migrate/20140916173139_add_publication_delay_to_user.rb
@@ -1,8 +1,10 @@
 class AddPublicationDelayToUser < ActiveRecord::Migration
   def up
     add_column :users, :publication_delay, :integer, default: 0, null: false
-    User.all.select{|u| u.has_role? Role.submitter}.each do |u|
-      u.email == 'admin@google.com' ? u.publication_delay = 10 * 24 * 60 * 60 : 0
+    User.all.each do |u|
+      if ['admin@google.com', 'google@chillingeffects.org'].include?(u.email)
+        u.update_attribute(publication_delay: 10 * 24 * 60 * 60)
+      end
     end
   end
 
