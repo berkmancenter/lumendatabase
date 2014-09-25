2b59525127bacac63ad5b7de02855473360c0880
jdcc <jclark@cyber.law.harvard.edu>
2014-09-25 16:54:59 -0400

    fix typo

--- db/migrate/20140916173139_add_publication_delay_to_user.rb
+++ db/migrate/20140916173139_add_publication_delay_to_user.rb
@@ -3,7 +3,7 @@ class AddPublicationDelayToUser < ActiveRecord::Migration
     add_column :users, :publication_delay, :integer, default: 0, null: false
     User.all.each do |u|
       if ['admin@google.com', 'google@chillingeffects.org'].include?(u.email)
-        u.update_attribute(publication_delay: 10 * 24 * 60 * 60)
+        u.update_attribute(:publication_delay, 10 * 24 * 60 * 60)
       end
     end
   end
