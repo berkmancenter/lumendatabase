12a6543e6cd814006849e28911d12992394078d0
Anita Patel <anita@anitapatel.com>
2015-03-25 15:19:31 -0400

    limiting records returned to 25 for non-researchers with API tokens

--- lib/custom_limiter.rb
+++ lib/custom_limiter.rb
@@ -1,14 +1,15 @@
 class CustomLimiter < Rack::Throttle::Minute
   def allowed?(request)
-    path_info = Rails.application.routes.recognize_path request.url rescue {}
+    #path_info = Rails.application.routes.recognize_path request.url rescue {}
     if request.media_type == "application/json"
+      Rails.logger.info request.env["HTTP_AUTHENTICATION_TOKEN"]
       if request.env["HTTP_AUTHENTICATION_TOKEN"].nil?
-        request.params["size"] = 25
+        request.GET[:per_page] = 25
         super
       elsif User.find_by_authentication_token(request.env["HTTP_AUTHENTICATION_TOKEN"]).has_role?(Role.researcher) 
         true
       else
-        request.params["size"] = 25
+        request.GET[:per_page] = 25
         super  
       end  
     else 
