9fafee5f9fd180f32f167a382e54c02f85153e09
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-31 13:53:29 -0400

    #12257 - more general email regex

--- app/models/redacts_notices.rb
+++ app/models/redacts_notices.rb
@@ -71,7 +71,7 @@ class RedactsNotices
   class RedactsEmail
     def redact(text)
       redactor = RedactsContent.new(
-        /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+        /\S+@\S+\.\S+/i
       )
 
       redactor.redact(text)
