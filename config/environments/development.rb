b33946d51d77703b66ca041b6a3b6eb0abbfbf27
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-09-11 15:00:56 -0400

    #12666 - don't fail if throttle cache is in use when running rake tasks

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,11 @@ Chill::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  #config.middleware.use Rack::Throttle::Minute, :max => 3, :cache => GDBM.new('tmp/throttle.db')
-  #Set up rate limiting
-  config.require "custom_limiter"
-  config.middleware.use CustomLimiter, :max => 3, :cache => GDBM.new('tmp/throttle.db')
+  begin
+    # Set up rate limiting
+    config.require "custom_limiter"
+    config.middleware.use CustomLimiter, :max => 3, :cache => GDBM.new('tmp/throttle.db')
+  rescue Errno::EAGAIN => e
+    # throttle cache in use by app, this is likely a rake task
+  end
 end
