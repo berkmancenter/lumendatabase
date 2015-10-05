cf9b79411e32a117b00d4850b4daed7fc87f5f11
Kent K <kent@endpoint.com>
2015-10-04 23:09:38 -0700

    Tweak migrations to rename url columns instead of replace them.
    Allows us to bring the site back up faster.

--- lib/default_url_original.rb
+++ lib/default_url_original.rb
@@ -8,4 +8,13 @@ module DefaultUrlOriginal
   def set_url_original
     self.url_original = url if self.url_original.nil?
   end
+
+  #TODO: Remove this once a copying script has been added and run.
+  def url
+    if super
+      super
+    else
+      url_original
+    end
+  end
 end
