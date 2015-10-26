6a2d8d1874d49963e399ce3f97f0262abad7c623
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-10-26 16:28:21 -0400

    default dev log to :info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Chill::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
+  # See everything in the log (default is :info)
+  config.log_level = :info
+
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
