24bf476c2d980d3f859cca7dab4f9978f112e793
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-02-22 11:44:58 -0500

    Fix to prevent creeating blank original notices
    This fixes the issue with the pull request where the lack of a reject_if
    block was causing the creation of a blank file_upload if someone
    attached a supporting document but not an original. Admins can now mark
    pdf_requests as fulfilled from RailsAdmin.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -122,7 +122,8 @@ class Notice < ActiveRecord::Base
 
   acts_as_taggable_on :tags, :jurisdictions
 
-  accepts_nested_attributes_for :file_uploads
+  accepts_nested_attributes_for :file_uploads,
+    reject_if: ->(attributes) { [attributes['file'], attributes[:pdf_request_fulfilled]].all?(&:blank?) }
 
   accepts_nested_attributes_for :entity_notice_roles
 
