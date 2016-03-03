b2857350234e2014716bf095b1386ae5f247fbab
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-03-03 16:37:11 -0500

    Include url_originals in the search index
    When querying from the API the results from a search query only include
    what is in the elasticsearch index as opposed to what is in the model.
    This update includes the url_originals in the search index. A subsequent
    commit will then require a researcher token to see url_originals.

--- app/models/searchability.rb
+++ app/models/searchability.rb
@@ -54,8 +54,8 @@ module Searchability
             notice.works.as_json({
               only: [:description] - exclusions[:works],
               include: {
-                infringing_urls: { only: [:url] },
-                copyrighted_urls: { only: [:url]}
+                infringing_urls: { only: [:url, :url_original] },
+                copyrighted_urls: { only: [:url, :url_original]}
               }
             })
           }
