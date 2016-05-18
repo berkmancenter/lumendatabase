1edddb7b1ffc4fd2b7adeedfedde4629d7054530
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-05-18 14:08:37 -0400

    Hide senders in defamation notices sent to Google
    * Uses same code as in the other.rb model

--- app/models/defamation.rb
+++ app/models/defamation.rb
@@ -1,4 +1,6 @@
 class Defamation < Notice
+	MASK = "REDACTED"
+	REDACTION_REGEX = /google/i
 
   define_elasticsearch_mapping(works: [:description])
 
@@ -8,6 +10,26 @@ class Defamation < Notice
 
   def to_partial_path
     'notices/notice'
+  end
+
+  def sender_name
+    if hide_identities?
+      MASK
+    else
+      super
+    end
+  end
+
+  def principal_name
+    if hide_identities?
+      MASK
+    else
+      super
+    end
+  end
+
+  def hide_identities?
+    recipient_name =~ REDACTION_REGEX
   end
 
 end
