80d8c2091434140388e23733791fe24cf19a2327
Emanuel Hayford <hi@siaw.co>
2016-06-08 07:50:12 +0200

    Update config.eager_load options

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,6 @@
 Chill::Application.configure do
+  
+  config.eager_load = false
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
