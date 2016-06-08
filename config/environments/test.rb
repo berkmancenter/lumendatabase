6797881e442217f77933294950291f395298018a
Emanuel Hayford <hi@siaw.co>
2016-06-08 07:54:31 +0200

    Remove mass assignment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 Chill::Application.configure do
-  
+
   config.eager_load = false
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -32,7 +32,7 @@ Chill::Application.configure do
   config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
