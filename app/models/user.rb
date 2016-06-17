e496289599508bd487039f135c967f5a80c0b91e
Emanuel Hayford <hi@siaw.co>
2016-06-17 09:12:24 +0200

    token_authenticatable has been removed from recent versions of Devise to protect against timing attacks. The reason for the removal has been given in this link: http://blog.plataformatec.com.br/2013/08/devise-3-1-now-with-more-secure-defaults/.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable,
-    :token_authenticatable, # API authentication
     :recoverable,           # New users are given a temp password to reset
     :validatable            # Ensures confirmation of Password on reset
 
   has_one :entity
   has_and_belongs_to_many :roles
   accepts_nested_attributes_for :entity
-
-  before_save :ensure_authentication_token
 
   def has_role?(role)
     self.roles.include?(role)
