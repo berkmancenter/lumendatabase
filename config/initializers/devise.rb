9317db0cfe1f64aa2cbc504bd945ef0913bb3824
Emanuel Hayford <hi@siaw.co>
2016-06-17 12:31:17 +0200

    Require 'devise/orm/active_record' before configs.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,15 +1,10 @@
 Devise.setup do |config|
+	require 'devise/orm/active_record'
+
   config.mailer_sender = "admin@chillingeffects.org"
-
-  require 'devise/orm/active_record'
-
   config.skip_session_storage = [:http_auth]
-
   config.stretches = Rails.env.test? ? 1 : 10
-
   config.password_length = 8..128
-
   config.email_regexp = /\A[^@]+@[^@]+\z/
-
   config.reset_password_within = 6.hours
 end
