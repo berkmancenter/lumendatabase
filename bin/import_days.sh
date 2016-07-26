845ec28f05413efa3f16b1da2539d2bf5a5b91af
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-07-26 17:18:15 -0400

    #13526 - bad request format

--- bin/import_days.sh
+++ bin/import_days.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 START=`echo $1 | tr -d -`;
 
 for (( c = 0; c < $2; c++ ))
 do
-  IMPORT_NAME=`date --date="$START +$c day" +%Y_%m_%d`;
+  IMPORT_NAME=`date --date="$START +$c day" +%Y-%m-%d`;
   echo $IMPORT_NAME;
-  `SKIP_RAILS_ADMIN_INITIALIZER=true IMPORT_NAME="$IMPORT_NAME" WHERE="tNotice.add_date = '$IMPORT_NAME' or tNotice.alter_date = '$IMPORT_NAME'" rake chillingeffects:import_notices_via_mysql`
+  `SKIP_RAILS_ADMIN_INITIALIZER=true IMPORT_NAME="$IMPORT_NAME" WHERE="tNotice.add_date like '$IMPORT_NAME%' or tNotice.alter_date like '$IMPORT_NAME%'" rake chillingeffects:import_notices_via_mysql`
 done
 
