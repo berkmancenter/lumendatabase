1e71036358e0e7410c2dfe3e6725ad496e89678d
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-07-26 14:44:23 -0400

    #13526 - disable rails admin during rake script

--- bin/import_days.sh
+++ bin/import_days.sh
@@ -5,6 +5,6 @@ for (( c = 0; c < $2; c++ ))
 do
   IMPORT_NAME=`date --date="$START +$c day" +%Y_%m_%d`;
   echo $IMPORT_NAME;
-  `IMPORT_NAME="$IMPORT_NAME" WHERE="tNotice.add_date = '$IMPORT_NAME' or tNotice.alter_date = '$IMPORT_NAME'" rake chillingeffects:import_notices_via_mysql`
+  `SKIP_RAILS_ADMIN_INITIALIZER=true IMPORT_NAME="$IMPORT_NAME" WHERE="tNotice.add_date = '$IMPORT_NAME' or tNotice.alter_date = '$IMPORT_NAME'" rake chillingeffects:import_notices_via_mysql`
 done
 
