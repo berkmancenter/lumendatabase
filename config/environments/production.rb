def6bffd24c27d73309581a74f1ff79296d82a25
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-02-03 12:06:35 -0500

    #14080 - rename config option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Chill::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Disable Rails's static files server (Apache or nginx will already do this)
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
