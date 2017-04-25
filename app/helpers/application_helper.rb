310847afbc0e10a516455c037803dc53ba4ea8a4
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-04-25 15:28:33 -0400

    at least provide link to edit in admin which works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,6 +26,12 @@ module ApplicationHelper
     rails_admin.show_path(model_name: model_name, id: object.id)
   end
 
+  def admin_edit_path(object)
+    model_name = RailsAdmin::AbstractModel.new(object.class).to_param
+
+    rails_admin.edit_path(model_name: model_name, id: object.id)
+  end
+
   def active_advanced_search_parameters?
     Notice::SEARCHABLE_FIELDS.find { |field| params[field.parameter] }.present?
   end
