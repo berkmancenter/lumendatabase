b6f7a8728132d8f8772ad119c04fe5c366bec7d1
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2017-07-25 15:33:22 -0400

    #14512 - use rendered in latest rspec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe 'home/index.html.erb' do
-  subject { rendered }
-  
   let(:notices) { build_stubbed_list(:dmca, 3, date_received: 1.hour.ago) }
   let(:blog_entries) { build_stubbed_list(:blog_entry, 3) }
 
@@ -16,14 +14,14 @@ describe 'home/index.html.erb' do
 
   it 'shows metadata and a link to each of its notices' do
     notices.each do |notice|
-      should have_link( notice.title, href: notice_path(notice) )
-      should have_css( "#notice_#{notice.id} time", text: notice.date_received.to_s(:simple) )
+      expect( rendered ).to have_link( notice.title, href: notice_path(notice) )
+      expect( rendered ).to have_css( "#notice_#{notice.id} time", text: notice.date_received.to_s(:simple) )
     end
   end
 
   it 'shows recent blog entries' do
     blog_entries.each do |blog_entry|
-      should have_link( blog_entry.title, href: blog_entry_path(blog_entry) )
+      expect( rendered ).to have_link( blog_entry.title, href: blog_entry_path(blog_entry) )
     end
   end
 end
