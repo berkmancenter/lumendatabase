1ecfbb6a3adbcec434512651529f835e2da044f2
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-08-02 10:37:35 -0400

    Leave note to our future selves to aid in test debugging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,13 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.order = "random"
 
+  # If you need to see the order your specs are running in (i.e. to debug
+  # order-dependent test failures), uncomment the following. But you may find
+  # rspec --bisect more useful. (It may also need the --drb flag to work.)
+  # config.before :all do
+  #  puts "Running #{self.class.description}"
+  # end
+
   config.before :each, cache: true do
     allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache::MemoryStore.new)
   end
