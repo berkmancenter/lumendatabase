ce92ed735a2590c428e5ed22826856786af1020e
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-09-07 11:02:13 -0400

    Upgrade paperclip to 4.X
    This requires being explicit about the fact that we're not validating
    filetypes (or else actually validating them).
    
    There are other things we could change with the 3->4 transition, but
    it's not worth dumping substantial time into this since when we get to
    Rails 5.2 paperclip is deprecated in favor of activestorage anyway.

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -8,13 +8,17 @@ class FileUpload < ActiveRecord::Base
   # attr_protected :id, :pdf_requested, :pdf_request_fulfilled
   # attr_protected :id, as: :admin
 
-  validates_inclusion_of :kind, in: %w( original supporting )
+  validates_inclusion_of :kind, in: %w[original supporting]
   validates :kind, length: { maximum: 255 }
 
   belongs_to :notice
   has_attached_file :file,
-    path: ":rails_root/paperclip/:class/:attachment/:id_partition/:style/:filename",
-    url: "/:class/:attachment/:id/:id_partition/:style/:filename"
+    path: ':rails_root/paperclip/:class/:attachment/:id_partition/:style/:filename',
+    url: '/:class/:attachment/:id/:id_partition/:style/:filename'
+
+  # Paperclip did not require validations at the time this file was written,
+  # and it is not now clear what filetypes were intended to be allowed.
+  do_not_validate_attachment_file_type :file
 
   before_save :rename_file, if: ->(instance) { instance.file_name.present? }
   delegate :url, to: :file
@@ -37,5 +41,4 @@ class FileUpload < ActiveRecord::Base
     self.file_file_name = file_name.gsub(/[^a-z\d\.\- ]/i, '_')
     true
   end
-
 end
