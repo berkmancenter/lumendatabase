4cb04035d1029bb773c7be27ba8ac63194b7df37
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-09-10 13:53:42 -0400

    Minor refactor

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,14 +1,15 @@
 module TopicsHelper
   def construct_recent_notices
-    searcher = topic_notice_searcher(@topic.name)
+    $stdout.puts "Helper can see #{Notice.count} notices"
+    searcher = topic_notice_searcher
     searcher.search
     searcher.instances
   end
 
   private
 
-  def topic_notice_searcher(topic_name)
-    searcher = SearchesModels.new(topic: topic_name)
+  def topic_notice_searcher
+    searcher = SearchesModels.new(topic: @topic.name)
     searcher.register TermSearch.new(:topic, :topic_facet)
     searcher.sort_by = :date_received, :desc
     searcher
