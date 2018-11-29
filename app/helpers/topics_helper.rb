aeb5ecc0ee0db7ba91196727459d0796a7789a6d
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-11-29 18:15:11 -0500

    Postprocess search results in TopicsHelper
    This lets us pull only the metadata we need to display notice links,
    instead of instantiating large numbers of AR objects just to populate
    a sidebar.
    
    We need to stop using the notice partial since we no longer have a
    notice object. This runs a risk that the sidebar styling will drift
    out of sync with similar styling elsewhere on the site. I may track
    that down in a future commit if it's not too much work.

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,14 +1,23 @@
+# Postprocesses elasticsearch results for display on TopicsController#show.
 module TopicsHelper
-  def construct_recent_notices
+  NoticeMetadata = Struct.new(:title, :id)
+
+  # Pulls notice name and id from elasticsearch results (which is all we need
+  # in order to render the page). This lets us avoid potentially expensive AR
+  # instantiations.
+  def recent_notice_metadata
     searcher = topic_notice_searcher
-    searcher.search
-    searcher.instances
+    results = searcher.search
+    results.map { |r| NoticeMetadata.new(r._source.title, r._source.id) }
   end
 
   private
 
+  # Searches for notices relevant to the topic. Limits to 10 results maximum
+  # as this information will be populating a recent notice sidebar and
+  # therefore does not need to be comprehensive.
   def topic_notice_searcher
-    searcher = SearchesModels.new(topic: @topic.name)
+    searcher = SearchesModels.new(topic: @topic.name, size: 10)
     searcher.register TermSearch.new(:topic, :topic_facet)
     searcher.sort_by = :date_received, :desc
     searcher
