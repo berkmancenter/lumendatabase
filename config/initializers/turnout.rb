c0aa7ddae2d515a1f0f6e39e9b4101cdf21cef1d
Andromeda Yelton <andromeda.yelton@gmail.com>
2019-01-03 10:42:22 -0500

    Allow all paths required for admin to be usable
    And maintenance page to be correctly styled.

--- config/initializers/turnout.rb
+++ config/initializers/turnout.rb
@@ -1,5 +1,14 @@
 Turnout.configure do |config|
   config.maintenance_pages_path = 'app/views/pages'
   config.default_reason = 'Lumen is temporarily unavailable due to site upgrades.'
-  config.default_allowed_paths = ['^/admin/']
+  config.default_allowed_paths = [
+    '^/admin',
+    # The following are required for the admin to be usable.
+    '^/assets/',
+    '^/users/sign_in',
+    '^/users/sign_out',
+    # The following are required to style the maintenance page correctly.
+    '^/error_style.css',
+    '^/assets/logo_2x.png'
+  ]
 end
