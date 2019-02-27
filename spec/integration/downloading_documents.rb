06d0d2342dda79b72af17697ccc4155e61ad78cf
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-02-27 13:20:49 +0100

    Add more specs

--- spec/integration/downloading_documents.rb
+++ spec/integration/downloading_documents.rb
@@ -7,16 +7,59 @@ feature 'Downloading documents' do
   scenario 'will redirect to the root page when a user has no access to download a document', js: true do
     visit notice.file_uploads.first.url
 
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content('You are not allowed to download this document.')
+    expect_no_download
   end
 
-  scenario 'will download a document for a user with access', js: true do
+  scenario 'will download a document for a logged-in user with access', js: true do
     sign_in(create(:user, :admin))
 
     visit notice.file_uploads.first.url
 
-    expect(page.response_headers['Content-Type']).to include('application/pdf')
+    expect_download
+
+    sign_out
+
+    sign_in(create(:user, :notice_viewer))
+
+    expect_download
+  end
+
+  scenario 'will download a document for a guest user with a valid access token', js: true do
+    token_url = create(
+      :token_url,
+      notice: notice,
+      expiration_date: Time.now + 24.hours
+    )
+
+    visit notice.file_uploads.first.url + "&access_token=#{token_url.token}"
+
+    expect_download
+  end
+
+  scenario 'won\'t download a document for a guest user with an expired access token', js: true do
+    token_url = create(
+      :token_url,
+      notice: notice,
+      expiration_date: Time.now - 24.hours
+    )
+
+    visit notice.file_uploads.first.url + "&access_token=#{token_url.token}"
+
+    expect_no_download
+  end
+
+  scenario 'won\'t download a document for a logged-in user with no access', js: true do
+    sign_in(create(:user, :researcher))
+
+    visit notice.file_uploads.first.url
+
+    expect_no_download
+  end
+
+  scenario 'won\'t download a document for a guest user', js: true do
+    visit notice.file_uploads.first.url
+
+    expect_no_download
   end
 
   scenario 'will disable a document notification for a user with a correct token', js: true do
@@ -42,5 +85,17 @@ feature 'Downloading documents' do
 
     expect(page).to have_current_path(root_path)
     expect(page).to have_content('Token url was not found.')
+  end
+
+  private
+
+  def expect_download
+    expect(page.response_headers['Content-Type']).to include('application/pdf')
+  end
+
+  def expect_no_download
+    expect(page.response_headers['Content-Type']).to include('text/html')
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content('You are not allowed to download this document.')
   end
 end
