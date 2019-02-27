1fc6080c1a8b88b1bd0b9a84c06f52ace4d3ffd6
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-02-27 19:21:37 +0100

    Add test to application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -31,6 +31,15 @@ describe ApplicationHelper do
       expect(result).to eq(true)
     end
 
+    it 'returns true when the user is allowed to see the full notice version and the token is not valid' do
+      allow_any_instance_of(ApplicationHelper).to receive(:params).and_return({access_token: 'WHATEVER'})
+      allow_any_instance_of(ApplicationHelper).to receive(:can?).and_return(true)
+
+      result = can_see_full_notice_version?(notice)
+
+      expect(result).to eq(true)
+    end
+
     it 'returns false when the token is not valid and the user not allowed to see the full notice version' do
       allow_any_instance_of(ApplicationHelper).to receive(:params).and_return({access_token: 'WHATEVER'})
       allow_any_instance_of(ApplicationHelper).to receive(:can?).and_return(false)
