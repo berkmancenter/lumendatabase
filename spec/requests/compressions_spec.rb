5420ca35d799e7347c6ced0dc3d077a324c148f4
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-05-13 17:24:02 +0530

    removed unnecessary condition

--- spec/requests/compressions_spec.rb
+++ spec/requests/compressions_spec.rb
@@ -4,18 +4,19 @@ RSpec.describe "Compressions", type: :request do
   describe Rack::Deflater do
 
     it "compresses response if requested" do
-    	get root_path
-    	uncompressed_response_length = response.headers['Content-Length']
-    	['deflate','gzip'].each do|compression_method|
-    		get root_path, {}, { HTTP_ACCEPT_ENCODING: compression_method }
-    		expect(response.headers['Content-Encoding']).to eq(compression_method)
-    		expect(response.headers['Content-Length']).to be <= uncompressed_response_length
+    	[root_path, '/topics.json'].each do |request_url|
+	    	['deflate','gzip'].each do |compression_method|
+	    		get request_url, {}, { HTTP_ACCEPT_ENCODING: compression_method }
+	    		expect(response.headers['Content-Encoding']).to eq(compression_method)
+	    	end
     	end
     end
 
     it "does not compress if compression not requested / supported" do
-    	get root_path
-    	expect(response.headers['Content-Encoding']).to be_nil
+    	[root_path, '/topics.json'].each do |request_url|
+	    	get request_url
+	    	expect(response.headers['Content-Encoding']).to be_nil
+    	end
     end
   end
 end
