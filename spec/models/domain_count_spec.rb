d90f949109b511eee80c6a5bdbf734960530e123
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-06-13 15:48:38 +0530

    code cleanup and handling duplicate domains

--- spec/models/domain_count_spec.rb
+++ spec/models/domain_count_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe DomainCount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'update domain count' do
+        it 'increments the count of a domain that already exists' do
+            DomainCount.new(domain_name: 'twitter.com').save
+            DomainCount.update_count('twitter.com')
+            expect(DomainCount.find_by_domain_name('twitter.com').count).to eq(1)
+        end
+
+        it 'creates a new domain if the domain is not existant' do
+            DomainCount.update_count('google.com')
+            expect(DomainCount.find_by_domain_name('google.com')).not_to be_nil
+        end
+  end
 end
