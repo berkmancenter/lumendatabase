216793b39a770137dec87dbd85c3eeec0e04d35f
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-06-13 16:42:05 +0530

    fix indentation

--- app/models/domain_count.rb
+++ app/models/domain_count.rb
@@ -1,12 +1,12 @@
 class DomainCount < ActiveRecord::Base
 	def self.update_count(domain_list)
-    domain_list.each do |domain|
-      domain_count_instance = find_by_domain_name(domain)
+		domain_list.each do |domain|
+  		domain_count_instance = find_by_domain_name(domain)
   		if domain_count_instance.nil?
   			domain_count_instance = new(domain_name: domain)
-        domain_count_instance.save
+      	domain_count_instance.save
   		end
   		domain_count_instance.increment!(:count)
-    end
+  	end
 	end
 end
