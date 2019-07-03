eb984307720faf41ab9b966b5da3e91507a16ff2
Andromeda Yelton <andromeda.yelton@gmail.com>
2019-07-03 16:22:35 -0400

    Create placeholder notice
    We have enough requests for placeholder notices that we need a
    subtype and a partial dedicated to them.

---
+++ app/models/placeholder.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Placeholder notices are used when notice providers want to report every
+# takedown request of a particular class with, effectively, a form letter (e.g.
+# a generic response to law enforcement requests from a given jurisdiction).
+class Placeholder < Notice
+  def self.model_name
+    Notice.model_name
+  end
+
+  def self.label
+    'Placeholder'
+  end
+
+  def to_partial_path
+    'notices/placeholder'
+  end
+end
