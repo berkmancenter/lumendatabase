0912aa93672e04656121947ae757188b76f5c5e2
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-08-29 22:56:50 +0530

    basic tests

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe StatisticsController, type: :controller do
+	context 'as HTML' do
+		it 'renders notices successfully' do
+			get :notices
+			expect(response).to be_successful
+		end
+
+		it 'renders urls page successfully' do
+			get :infringing_urls
+			expect(response).to be_successful
+		end
+
+		it 'renders entities page successfully' do
+			get :entities
+			expect(response).to be_successful 
+		end
+	end
+
+	context 'as JSON' do
+		it 'renders json for datewise_notices' do
+			get :datewise_notices
+			expect(response).to be_successful
+		end
+
+		it 'renders json for datewise_urls' do
+			get :datewise_urls
+			expect(response).to be_successful
+		end
+	end
 
 end
