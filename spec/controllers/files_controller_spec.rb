6a26101a3e93dfdd403a9065bfa9c4b96eaa07f7
Andromeda Yelton <andromeda.yelton@gmail.com>
2019-11-15 10:04:03 -0500

    Address deprecation warning

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -5,13 +5,13 @@ describe FilesController do
     let(:upload) { create(:file_upload) }
     it 'returns not found without valid params' do
       get 'show', params: { id: upload.id, file_path: %w[a b c] }
-      expect(response).not_to be_success
+      expect(response).not_to be_successful
     end
 
     it 'returns http success' do
       allow(File).to receive(:file?).and_return(true)
       allow(File).to receive(:read).and_return('Content!')
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 end
