3ecc5776cda129b21a7436d9132695dfb2ce9e1e
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-03-27 12:59:46 -0400

    Improve URL validation * Refactor * Use more precise regexp

--- app/models/infringing_url.rb
+++ app/models/infringing_url.rb
@@ -1,12 +1,11 @@
-require 'validates_automatically'
 require 'default_url_original'
+require 'validates_automatically'
+require 'validates_urls'
 
 class InfringingUrl < ApplicationRecord
   include ValidatesAutomatically
   include DefaultUrlOriginal
-
-  validates_format_of :url, :url_original, with: /\A([a-z]{3,5}:)?\/\/.+/i
-  validates_presence_of :url
+  include ValidatesUrls
 
   def self.get_approximate_count
   	ActiveRecord::Base.connection.execute("SELECT reltuples FROM pg_class WHERE relname = 'infringing_urls'").getvalue(0, 0).to_i
