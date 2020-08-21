b45fbd2bf540c277b26ab76b3fc115c72bf6b418
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-08-21 14:56:54 -0400

    Make log level controllable via ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.log_level = (ENV['RAILS_LOG_LEVEL'] || :debug).to_sym
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
