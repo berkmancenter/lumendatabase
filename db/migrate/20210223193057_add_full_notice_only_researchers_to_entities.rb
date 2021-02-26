1b5be3d4fd727657b093fdc3fc31ca2c09afe9ae
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2021-02-26 12:16:46 +0100

    Move info text from notice view to CMS

--- db/migrate/20210223193057_add_full_notice_only_researchers_to_entities.rb
+++ db/migrate/20210223193057_add_full_notice_only_researchers_to_entities.rb
@@ -6,5 +6,12 @@ class AddFullNoticeOnlyResearchersToEntities < ActiveRecord::Migration[5.2]
     end
 
     add_column :entities, :full_notice_only_researchers, :boolean
+
+    Comfy::Cms::Snippet.create!(
+      identifier: 'lumen_notice_only_full_for_researchers',
+      label: 'Full for researchers only text on notice view',
+      content: 'Thanks for your interest, but URLs from submitter <span class="lumen-badge">###SUBMITTER_NAME###</span> are viewable only by users with a Lumen researcher credential.',
+      site: Comfy::Cms::Site.first
+    )
   end
 end
