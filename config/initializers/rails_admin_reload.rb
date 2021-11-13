ebc561e6ea7f252f21ae6462528af02d148af288
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2021-11-13 12:14:56 +0100

    Reload rails admin config only after certain actions

--- config/initializers/rails_admin_reload.rb
+++ config/initializers/rails_admin_reload.rb
@@ -1,8 +1,12 @@
 Rails.application.config.to_prepare do
   RailsAdmin::ApplicationController.class_eval do
-    before_action :reload_rails_admin
+    after_action :reload_rails_admin
 
     def reload_rails_admin
+      action_to_reload_after = ['top_notices_token_urls']
+
+      return unless action_to_reload_after.include? action_name
+
       RailsAdmin::Config.reset
 
       load("#{Rails.root}/config/initializers/rails_admin.rb")
