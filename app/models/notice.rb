c063b34a88143c649f45eeb10e6a59f5f1a82fd6
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2022-03-25 13:17:14 +0100

    Update activestorage gem

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -18,7 +18,8 @@ class Notice < ApplicationRecord
     TermSearch.new(:topics, 'topics.name', 'Topics'),
     TermSearch.new(:tags, :tag_list, 'Tags'),
     TermSearch.new(:jurisdictions, :jurisdiction_list, 'Jurisdictions'),
-    TermSearch.new(:entities_country_codes, :entities_country_codes, 'Entity Country Code'),
+    # It's not fully indexed in
+    # TermSearch.new(:entities_country_codes, :entities_country_codes, 'Entity Country Code'),
     TermSearch.new(:sender_name, :sender_name, 'Sender Name'),
     TermSearch.new(:principal_name, :principal_name, 'Principal Name'),
     TermSearch.new(:recipient_name, :recipient_name, 'Recipient Name'),
