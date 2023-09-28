c931d4e493985faab85c9e5c7b796bb573528bed
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-09-28 16:53:26 +0200

    Use ActiveJob in ProxyCache

---
+++ app/jobs/clear_cache_job.rb
@@ -0,0 +1,14 @@
+class ClearCacheJob < ApplicationJob
+  queue_as :default
+
+  def perform(url)
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Host'] = uri.host
+    request[ProxyCache::CLEAR_HEADER] = 'yolo'
+
+    http.request(request)
+  end
+end
--- spec/lib/proxy_cache.rb
+++ spec/lib/proxy_cache.rb
@@ -1,85 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe ProxyCache do
-  before do
-    ENV['PROXY_CACHE_CLEAR_HEADER'] = 'LUMEN_CLEAR_PROXY_CACHE'
-    ProxyCache.send(:remove_const, 'CLEAR_HEADER')
-    ProxyCache.const_set('CLEAR_HEADER', 'LUMEN_CLEAR_PROXY_CACHE')
-  end
-
   describe '.clear_notice' do
-    context 'when CLEAR_HEADER is set' do
-      it 'calls call method when notice_ids is not an array' do
-        expect(ProxyCache).to receive(:call).with(instance_of(String))
-        ProxyCache.clear_notice(123)
-      end
+    it 'clears cache for a single notice' do
+      notice_id = 1
 
-      it 'calls call_many method when notice_ids is an array' do
-        expect(ProxyCache).to receive(:call_many).with([123, 456])
-        ProxyCache.clear_notice([123, 456])
-      end
+      expect(ClearCacheJob).to receive(:set).with(wait: 0.seconds).and_return(ClearCacheJob)
+      expect(ClearCacheJob).to receive(:perform_later).with(notice_url(notice_id))
+
+      ProxyCache.clear_notice(notice_id)
     end
 
-    context 'when CLEAR_HEADER is not set' do
-      before do
-        ENV['PROXY_CACHE_CLEAR_HEADER'] = nil
-        ProxyCache.send(:remove_const, 'CLEAR_HEADER')
-        ProxyCache.const_set('CLEAR_HEADER', nil)
+    it 'clears cache for multiple notices' do
+      notice_ids = [1, 2, 3]
+      i= 0 
+
+      notice_ids.each do |notice_id|
+        n_delay = i * ProxyCache::REQUEST_SLEEP_SECONDS
+        expect(ClearCacheJob).to receive(:set).with(wait: n_delay.seconds).and_return(ClearCacheJob)
+        expect(ClearCacheJob).to receive(:perform_later).with(notice_url(notice_id))
+
+        i += 1
       end
 
-      it 'does not call any methods' do
-        expect(ProxyCache).not_to receive(:call)
-        expect(ProxyCache).not_to receive(:call_many)
-        ProxyCache.clear_notice(123)
-      end
+      ProxyCache.clear_notice(notice_ids)
+    end
+
+    it 'does not clear cache if CLEAR_HEADER is not set' do
+      ENV['PROXY_CACHE_CLEAR_HEADER'] = nil
+      ProxyCache.send(:remove_const, 'CLEAR_HEADER')
+      ProxyCache.const_set('CLEAR_HEADER', nil)
+
+      expect(ProxyCache).not_to receive(:call)
+
+      ProxyCache.clear_notice(1)
     end
   end
 
   describe '.call' do
-    it 'sends an HTTP GET request with the given URL and timeout' do
-      url = 'https://example.com'
-      timeout = 1
+    it 'enqueues a ClearCacheJob with the given URL and delay' do
+      id = 1
 
-      uri = URI(url)
-      http = instance_double(Net::HTTP)
-      request = instance_double(Net::HTTP::Get)
+      expect(ClearCacheJob).to receive(:set).with(wait: 0.seconds).and_return(ClearCacheJob)
+      expect(ClearCacheJob).to receive(:perform_later).with(notice_url(id))
 
-      allow(Net::HTTP).to receive(:new).with(uri.host, uri.port).and_return(http)
-      allow(http).to receive(:use_ssl=).with(true)
-      allow(Net::HTTP::Get).to receive(:new).with(uri.request_uri).and_return(request)
-  
-      expect(request).to receive(:[]=).with('Host', uri.host)
-      expect(request).to receive(:[]=).with('LUMEN_CLEAR_PROXY_CACHE', 'yolo')
-
-      expect(http).to receive(:request).with(request).and_return(Net::HTTPSuccess.new(nil, nil, nil))
-
-      ProxyCache.call(url, timeout)
-
-      sleep(2)
+      ProxyCache.call(notice_url(1))
     end
   end
 
   describe '.call_many' do
-    it 'calls the call method for each notice_id with increasing sleep time' do
-      notice_ids = [123, 456, 789]
-      i = 1
-      notice_ids.each do |notice_id|
-        expected_timeout = i * 5
-        expect(ProxyCache).to receive(:call).with(instance_of(String), expected_timeout)
+    it 'enqueues ClearCacheJob for multiple URLs with the specified delay' do
+      notice_ids = [1, 2, 3]
+      i = 0
+
+      notice_ids.each do |id|
+        n_delay = i * ProxyCache::REQUEST_SLEEP_SECONDS
+        expect(ClearCacheJob).to receive(:set).with(wait: n_delay.seconds).and_return(ClearCacheJob)
+        expect(ClearCacheJob).to receive(:perform_later).with(notice_url(id))
+
         i += 1
       end
+
       ProxyCache.call_many(notice_ids)
     end
   end
 
   describe '.notice_url' do
     it 'generates the correct notice URL' do
-      notice_id = 123
-      expect(Rails.application.routes.url_helpers).to receive(:notice_url).with(
-        host: Chill::Application.config.site_host,
-        id: notice_id
-      )
-      ProxyCache.notice_url(notice_id)
+      notice_id = 42
+      expected_url = "#{SITE_URL}/notices/#{notice_id}"
+
+      generated_url = ProxyCache.notice_url(notice_id)
+
+      expect(generated_url).to eq(expected_url)
     end
+  end
+
+  private
+
+  def notice_url(id)
+    Rails.application.routes.url_helpers.notice_url(host: ENV['SITE_HOST'], id: id)
   end
 end
