23eb88071846f8cb558354db58ffb93bc0bdc71a
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-09-28 23:40:32 +0200

    Initial sidekiq configuration

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['SIDEQIK_REDIS_URL'] || 'redis://localhost:6379/1' }
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['SIDEQIK_REDIS_URL'] || 'redis://localhost:6379/1' }
+
+Rails.application.config.active_job.queue_adapter = :sidekiq
+
+# nil will use the "default" queue
+# https://github.com/sidekiq/sidekiq/wiki/Active-Job#queues
+Rails.application.config.action_mailer.deliver_later_queue_name = nil
+Rails.application.config.action_mailbox.queues.routing = nil
+Rails.application.config.active_storage.queues.analysis = nil
+Rails.application.config.active_storage.queues.purge = nil
+Rails.application.config.active_storage.queues.mirror = nil
