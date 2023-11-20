d7d793933a4c914f1a0122f405fcb3f1006ac4ca
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-11-20 16:47:25 +0100

    Refactor GoogleSenderRedactor class

--- lib/redactors/google_sender_redactor.rb
+++ lib/redactors/google_sender_redactor.rb
@@ -4,10 +4,23 @@ class GoogleSenderRedactor
   def redact(instance)
     return unless (instance.recipient_name =~ REDACTION_REGEX).present?
 
-    instance.sender.name_original = instance.sender.name
-    instance.principal.name_original = instance.principal.name
-    instance.sender.name = Lumen::REDACTION_MASK
-    instance.principal.name = Lumen::REDACTION_MASK
-    instance.save
+    need_save = redact_entity(instance.sender) || redact_entity(instance.principal)
+
+    instance.save if need_save
+  end
+
+  private
+
+  def redact_entity(entity)
+    if entity.name != Lumen::REDACTION_MASK
+      entity.name_original = entity.name
+      entity.name = Lumen::REDACTION_MASK
+      true
+    else
+      unless entity.name_original.present?
+        entity.name_original = Lumen::REDACTION_MASK
+        true
+      end
+    end
   end
 end
