ea912df313efa9f06947678baa375a8b9226f02a
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2023-12-06 19:19:48 +0100

    Don't verify SSL certificate in ClearCacheJob

--- app/jobs/clear_cache_job.rb
+++ app/jobs/clear_cache_job.rb
@@ -5,6 +5,7 @@ class ClearCacheJob < ApplicationJob
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == 'https')
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     request['Host'] = uri.host
     request[ProxyCache::CLEAR_HEADER] = 'yolo'
