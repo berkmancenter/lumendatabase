849405723cf0d66cde8d7fff037b0e41cf1a2cf7
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-09-03 23:33:38 +0200

    Make import_github_notices_spec pass in test suite

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,7 +5,4 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.default_cassette_options = {
-    allow_playback_repeats: true
-  }
 end
