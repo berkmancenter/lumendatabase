c01699ddd474bb6b4a4abe2ebdf6e3728b6dfe7b
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2026-09-08 22:38:00 +0200

    Decrease submission attachment size

--- lib/lumen/submissions/attachment.rb
+++ lib/lumen/submissions/attachment.rb
@@ -7,8 +7,8 @@ module Lumen::Submissions::Attachment
 
   DEFAULT_KIND = 'supporting'
   MAX_ATTACHMENTS = 500
-  MAX_ATTACHMENT_BYTES = 50.megabytes
-  MAX_TOTAL_ATTACHMENT_BYTES = 200.megabytes
+  MAX_ATTACHMENT_BYTES = 25.megabytes
+  MAX_TOTAL_ATTACHMENT_BYTES = 100.megabytes
   DATA_URI_PATTERN = %r{\Adata:([^;,]+);base64,(.*)\z}m
 
   module_function
